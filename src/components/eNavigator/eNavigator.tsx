b4e27e090336d0dc0ed099a3f2edf5cb95021c60
Manuel Garcia <mgarcia@rmgperustaff.com>
2020-05-25 17:00:38 -0500

    Update: Add theming based in the color palette

--- src/components/eNavigator/eNavigator.tsx
+++ src/components/eNavigator/eNavigator.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const ENavigator: React.FC = () => {
-  return <div className="Navigator"></div>;
-};
+const ENavigator: React.FC = () => (
+  <div className="Navigator">
+    <h2>From Nav</h2>
+  </div>
+);
 
 export default ENavigator;
